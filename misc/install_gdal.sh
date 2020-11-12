e4f53ff79c8c229e4f5715649defec2c4ef7d1f9
mthh <matthieu.viry@univ-grenoble-alpes.fr>
2020-11-12 18:19:01 +0100

    Update gdal dependencie in install files

--- misc/install_gdal.sh
+++ misc/install_gdal.sh
@@ -31,12 +31,12 @@ if [ ! -d "$GDALINST" ]; then
   mkdir $GDALINST;
 fi
 
-if [ ! -d $GDALINST/gdal-2.4.2 ]; then
+if [ ! -d $GDALINST/gdal-3.0.4 ]; then
   cd $GDALBUILD
-  wget http://download.osgeo.org/gdal/2.4.2/gdal-2.4.2.tar.gz
-  tar -xzf gdal-2.4.2.tar.gz
-  cd gdal-2.4.2
-  ./configure --prefix=$GDALINST/gdal-2.4.2 $GDALOPTS
+  wget http://download.osgeo.org/gdal/3.0.4/gdal-3.0.4.tar.gz
+  tar -xzf gdal-3.0.4.tar.gz
+  cd gdal-3.0.4
+  ./configure --prefix=$GDALINST/gdal-3.0.4 $GDALOPTS
   make -s -j 2
   make install
 fi
