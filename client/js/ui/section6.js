c5a532086e0078100a60c578216206d79c1a3f84
armel <vidali.armel@gmail.com>
2023-03-28 18:28:47 +0200

    documentation, corrections, manques les dernières fonctions pour enregistrer les filtres choisis pendant la navigation

--- client/js/ui/section6.js
+++ client/js/ui/section6.js
@@ -6,13 +6,28 @@ export function makeSection6() {
   const section6 = d3.select('#section6');
   const dv6 = section6.append('div');
 
-  const layer_selection = dv6.append('div');
 
+  dv6
+  .append("button")
+  .on("click", function(){
+    get_index_checked_box();
+    reset_filter();
+    })
+  .style("text-align", "center")
+  .style("display", "flex")
+  .style("justify-content", "center")
+  .style("align-items", "center")
+  .text("Reinitialiser")
+  
+
+  const layer_selection = dv6.append('div');
   layer_selection.styles({
     "display" : "flex",
     "flex-direction" : "column",
     "justify-content" : "center"
   })
+
+
 
   /* section choix de la couche */
   layer_selection.append('p')
@@ -86,13 +101,16 @@ export function makeSection6() {
 
 /* armel : fonction pour peupler le menu de selection avec la list des couches présentent sur la carte */
 export function update_section_6(){
-
+    /**
+     * Fonction pour pallier à la selection D3 qui n'est pas live 
+     */
     function return_d3_live_selection(id){
       var element = d3.select(`#${id}`)
       return element
     }
     
     const options = []
+    // A l'execution, rajoute la liste des couches présentes au menu déroulant des couches
     for(let i = 0; i < document.getElementById("layer_choice").options.length; i++   ){
         options.push(document.getElementById("layer_choice").options[i].text)
     }
@@ -109,8 +127,6 @@ export function update_section_6(){
                 'id' : `layer_name_${layer_name}`
             })
             .text(layer_name)}   
-             
-
     }
 
 
@@ -128,18 +144,25 @@ export function update_section_6(){
         .selectAll('option')
         .on("click" , function(){
 
+            //au changement de couches, suppression des champs et valeurs de la couche précedente
             return_d3_live_selection("field_choice")
                 .selectAll('option')
                 .remove()
 
+            return_d3_live_selection("value_choice")
+              .selectAll("li")
+              .remove()    
+            // valeur du menu choix de couche
             let option_text = this.text
+            // liste des champs disponibles pour chaque couche
             let original_fields =  Array.from(data_manager.current_layers[option_text].original_fields)
 
+            //ajout des chamsp au menu déroulant
             for(let i = 0; i < original_fields.length; i ++){
-
               return_d3_live_selection("field_choice")
                     .append("option")
                     .text(original_fields[i])
+            // mise a jour du menu, je suis pas sur pourquoi ca fonctionne
             update_section_6()
             }
         }
@@ -149,15 +172,17 @@ export function update_section_6(){
     return_d3_live_selection("field_choice")
         .selectAll('option')
         .on("click" , function(){
-           
+            // retire toutes les valeurs du champs/couche précédente
             return_d3_live_selection("value_choice").selectAll("li").remove()
-
+            //couche selectionnée
             let choosed_layer = return_d3_live_selection("layer_choice")
-
+            //récupère les valeurs uniques pour chaque champ
             var unique_values = get_unique_value(choosed_layer.property("value"))
-            
 
-            for(let value of unique_values[this.text]){
+          
+            
+            //ajout des valeurs
+            for(let value of unique_values[this.text].sort()){
               return_d3_live_selection("value_choice")
                     .append("li")
                     .styles({
@@ -166,60 +191,51 @@ export function update_section_6(){
                     .text(value)
                     .append("input")
                     .attrs({"type" :"checkbox", "value" : value})
-                    
-
             }
-            
+          //maj du menu            
          update_section_6()   
         }
     ) 
 
+    //filtrage des données après click sur les checkbox
     return_d3_live_selection("value_choice")
         .selectAll("input")
         .on("click",  function(){
 
+          //couche selectionnée
           var choosed_layer = return_d3_live_selection("layer_choice").property("value") 
 
-
+          //id de chaque entitées à filtrer 
           let shapes_to_filter = filter_values(
             choosed_layer,
             return_d3_live_selection("field_choice").property("value"), this.value
             )
-
-          
-
-
-
-          for(let shape of shapes_to_filter){            
-
-            console.log(choosed_layer, shape);
-            let filtered_shape =  document.querySelector(`#L_${choosed_layer} #feature_${shape}`)
-
-            
+          // pour chaque ID
+          for(let shape of shapes_to_filter){ 
+            // selection par ID compris à l'interrieur un groupe de path (une couche sur l'UI)
+            let filtered_shape =  document.querySelector(`#L_${choosed_layer} #feature_${shape}`)           
             if(this.checked == true){
               filtered_shape.setAttribute("display", "none")
             }
             else{
               filtered_shape.setAttribute("display" , "")
-            }
-            
+            }            
           }  
         } 
-        )
-
-
-
-
-
-
+     )
 }
 
+/**
+ * Récupère la liste des valeurs unique pour chaque champs pour une couche données depuis le 
+ * data manager.
+ * @param layer : nom de la couche 
+ * @returns Object : key = champ, value = liste de valeurs uniques
+ */
 function get_unique_value(layer){
-  var user_data = data_manager.user_data[layer]
   var user_values = {}
-  var original_fields = data_manager.current_layers[layer].original_fields
 
-  
+  var user_data = data_manager.user_data[layer]
+  var original_fields = data_manager.current_layers[layer].original_fields
 
   //pour chaque champ de la couche
   for(let current_field of original_fields){
@@ -227,18 +243,18 @@ function get_unique_value(layer){
     let unique_values = new Set()
     for(let i  = 0; i < user_data.length ; i++){
         unique_values.add(user_data[i][current_field])
-        
-    }
-    
+    }    
     user_values[current_field] = Array.from(unique_values)
-  }
- 
-  console.log(user_values);
-  // retourne un objet avec clef = nom du champ, valeur = set de values
+  } 
   return user_values
 }
 
-
+/**
+ * Récupère la liste des id des entités donc la valeur d'un champ donné match la valeur selectionnée 
+ * par l'utilisateur pour masquer
+ * 
+ * @returns liste d'ID d'entités a filtrer 
+ */
 function filter_values(layer, field, value){
   var user_data = data_manager.user_data[layer]
   var shapes_to_filter = []
@@ -247,10 +263,62 @@ function filter_values(layer, field, value){
     if (user_data[i][field] == value){
       shapes_to_filter.push(i)
     }
+  }
+  return shapes_to_filter
+}
 
+
+
+/**
+ * Reinitialise tous les filtres pour tout afficher
+ */
+
+function reset_filter(){
+  var value_choice = document.querySelectorAll("#value_choice input") 
+  for(let checkbox of value_choice){
+    checkbox.checked = false
+  }
+  var svg_map = document.getElementById("svg_map")
+
+  // pour chaque couche de la carte
+  for(let node of svg_map.childNodes){
+    if(node.nodeName == "defs"){/* do nothing */}
+    else{
+      // pour chaque svg de chaque couche
+      for(let path of node.childNodes){
+        path.setAttribute("display" , "")
+      }
+    }
+  }
+}
+
+/**
+ * Récupère toutes les checkbox cochées pour pouvoir les garder affichées en naviguant dans les couches
+ */
+function get_index_checked_box(){
+  var value_choice = document.querySelectorAll("#value_choice input") 
+  var seleced_layer = d3.select("€layer_choice").property("value")
+  var checked_inputs = []
+
+  for(let i = 0; i < value_choice.length ; i++){
+    if(value_choice[i].checked == true){
+      checked_inputs.push(i)
+    }
   }
 
+  localStorage.setItem("checked_inputs", checked_inputs)
+}
 
-  return shapes_to_filter
+/**
+ * Recheck tous les checkbox précédemment cochées
+ */
+function recheck_inputs(){
+  let checked_inputs = localStorage.getItem("checked_inputs")
+  var value_choice = document.querySelectorAll("#value_choice input") 
 
+  for(let i = 0; i < value_choice.length ; i++){
+    if(checked_inputs.includes(i)){
+    value_choice[i].checked == true}
+  }
+  
 }
